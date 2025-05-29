2aaf28e0c855ad8bace4202d38354b23d932c22c
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-05-29 15:57:02 +0200

    Create the merge function which will be used in the recursion

---
+++ mergeSort.js
@@ -0,0 +1,28 @@
+
+
+function merge(arrayLeft, arrayRight) {
+    // Create another array and two indexes
+    let resultArray = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    // Compare the indexes and add to the array dpending on the comparison
+    while(leftIndex < arrayLeft.length && rightIndex < arrayRight.length){
+        if(arrayLeft[leftIndex] < arrayRight[rightIndex]){
+            resultArray.push(arrayLeft[leftIndex]);
+            leftIndex++;
+        }else{
+            resultArray.push(arrayRight[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    // Concatenate the remaining elements from either left array or right array (if any)
+    return resultArray
+    .concat(arrayLeft.slice(leftIndex))
+    .concat(arrayRight.slice(rightIndex));
+}
+
+console.log(`Sorted array for [38, 27, 43, 3, 9, 82, 10] is ${mergeSort([38, 27, 43, 3, 9, 82, 10])}`);
+console.log(`Sorted array for [3, 2, 1, 13, 8, 5, 0, 1] is ${mergeSort([3, 2, 1, 13, 8, 5, 0, 1])}`);
+console.log(`Sorted array for [105, 79, 100, 110] is ${mergeSort([105, 79, 100, 110])}`);
