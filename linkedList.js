766a910b36694c4af95fd6b3c6773c5b0c552b93
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-06-03 19:48:10 +0200

    Add method that showcases the nodes in a string format

--- linkedList.js
+++ linkedList.js
@@ -1,107 +1,116 @@
-import { Node } from "./node.js";
+import { Node } from './node.js';
 
 function LinkedList() {
-    let head = null;
-    let tail = null;
-    let size = 0;
+  let head = null;
+  let tail = null;
+  let size = 0;
 
-    const append = (value) => {
-        const newNode = Node(value);
+  const append = (value) => {
+    const newNode = Node(value);
 
-        if (!head) {
-            head = newNode;
-            tail = newNode;
-        } else {
-            tail.nextNode = newNode;
-            tail = newNode;
-        }
-
-        size++;
+    if (!head) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      tail.nextNode = newNode;
+      tail = newNode;
     }
 
-    const prepend = (value) => {
-        const newNode = Node(value, head);
-        head = newNode;
-         if (!tail) {
-            tail = newNode;
-        }
+    size++;
+  };
 
-        size++;
+  const prepend = (value) => {
+    const newNode = Node(value, head);
+    head = newNode;
+    if (!tail) {
+      tail = newNode;
     }
 
-    const getSize = () => size;
-    const getHead = () => head;
-    const getTail = () => tail;
+    size++;
+  };
 
-    const getNodeAt = index => {
+  const getSize = () => size;
+  const getHead = () => head;
+  const getTail = () => tail;
 
-        let current = head;
-        let count = 0;
+  const getNodeAt = (index) => {
+    let current = head;
+    let count = 0;
 
-        if(!head) return -1;
+    if (!head) return -1;
 
-        while(current !== null){
-            if(count === index) return current.value;
-            count++;
-            current = current.nextNode;
-        }
-
-        return `Index out of bounds`;
+    while (current !== null) {
+      if (count === index) return current.value;
+      count++;
+      current = current.nextNode;
     }
 
-    const pop = () => {
-        if(!head) return null;
-        
-        if(!head.nextNode) {
-            const value = head.value;
-            head = null;
-            tail = null;
-            size--;
-            return value;
-            }
-        
-        let previous = head;
-        let node = head.nextNode;
-        
-        while (node.nextNode) {
-         previous = node;
-         node = node.nextNode;
-        }
-            
-        previous.nextNode = null;
-        tail = previous;
-        size--;
-        return node.value;
+    return `Index out of bounds`;
+  };
+
+  const pop = () => {
+    if (!head) return null;
+
+    if (!head.nextNode) {
+      const value = head.value;
+      head = null;
+      tail = null;
+      size--;
+      return value;
     }
 
-    const contains = value => {
-        let node = head;
-        
-        while (node) {
-         if(node.value === value) return true;
-         node = node.nextNode;
-        }
+    let previous = head;
+    let node = head.nextNode;
 
-         return false;
-
+    while (node.nextNode) {
+      previous = node;
+      node = node.nextNode;
     }
 
-    const find = value => {
-        let current = head;
-        let index = 0;
+    previous.nextNode = null;
+    tail = previous;
+    size--;
+    return node.value;
+  };
 
-        if(!head) return -1;
+  const contains = (value) => {
+    let node = head;
 
-        while(current !== null){
-            if(current.value === value) return index;
-            index++;
-            current = current.nextNode;
-        }
-
-        return null;
+    while (node) {
+      if (node.value === value) return true;
+      node = node.nextNode;
     }
 
-    
+    return false;
+  };
+
+  const find = (value) => {
+    let current = head;
+    let index = 0;
+
+    if (!head) return -1;
+
+    while (current !== null) {
+      if (current.value === value) return index;
+      index++;
+      current = current.nextNode;
+    }
+
+    return null;
+  };
+
+  const toString = () => {
+    let result = '';
+    let current = head;
+
+    while (current !== null) {
+      result += `( ${current.value} ) -> `;
+      current = current.nextNode;
+    }
+
+    result += 'null';
+    return result;
+  };
 }
 
 export { LinkedList };
